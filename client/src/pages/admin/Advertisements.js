ff88fef8ec332f3bb3d802e1ae875bef17a2dd8b
DanuTJ <59443951+DanuTJ@users.noreply.github.com>
2021-09-18 01:46:50 +0530

    Admin display ads

--- client/src/pages/admin/Advertisements.js
+++ client/src/pages/admin/Advertisements.js
@@ -1,5 +1,44 @@
 import React from 'react';
+import axios from "axios";
+import { useEffect, useState } from 'react';
+import Typography from '@material-ui/core/Typography';
+import "./css/Admin.css";
 
-export default function Advertisements() {
-  return <div>Ads</div>;
-}
+ function Advertisements() {
+  const [listofAds, setlistofAds] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/ads").then((response) => {
+       setlistofAds(response.data);
+    });
+  }, [])
+  return <div className="Ads">{listofAds.map((value, key) => {
+    return (
+       <div className="ad">
+      <Typography component="h6" variant="h5" align="left"> <div className="title">General Details</div></Typography>
+      <br></br>
+      <Typography component="h6" variant="h6" align="left" style={{ color: 'black' }}> Title: {value.name}</Typography> 
+      
+      <Typography component="h6" variant="h6" align="left" style={{ color: 'black' }}>Description: {value.description}</Typography>
+      
+      <Typography component="h6" variant="h6" align="left" style={{ color: 'black' }}>Unit Price: Rs.{value.price} </Typography>
+     
+      <Typography component="h6" variant="h6" align="left" style={{ color: 'black' }}>Quantity Available: {value.quantity} </Typography>
+      
+      <Typography component="h6" variant="h6" align="left" style={{ color: 'black' }}>Condition: {value.condition} </Typography>
+      
+      <Typography component="h6" variant="h6" align="left" style={{ color: 'black' }}>Category: {value.category} </Typography>
+      
+      <Typography component="h6" variant="h5" align="left"> <div className="title">Vehicle Details for the Spare Part</div></Typography>
+      <br></br>
+      <Typography component="h6" variant="h6" align="left" style={{ color: 'black' }}> Model: {value.vehicleModel} </Typography>
+
+      <Typography component="h6" variant="h6" align="left" style={{ color: 'black' }}> Manufactured Year: {value.year} </Typography>
+
+      <Typography component="h6" variant="h6" align="left" style={{ color: 'black' }}> Manufactured Country: {value.country} </Typography>
+      </div>
+  );
+  })} 
+  </div>  
+};
+export default Advertisements;
