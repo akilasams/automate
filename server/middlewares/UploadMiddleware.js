fb9296249f5efe199ca93af6573e0e197afa1e87
DanuTJ <59443951+DanuTJ@users.noreply.github.com>
2021-09-22 00:19:57 +0530

    shop approval

--- server/middlewares/UploadMiddleware.js
+++ server/middlewares/UploadMiddleware.js
@@ -5,6 +5,7 @@ const MIME_TYPE_MAP = {
   'image/png': 'png',
   'image/jpg': 'jpg',
   'image/jpeg': 'jpeg',
+  'application/pdf': 'pdf',
 };
 
 const fileUpload = multer({
@@ -18,11 +19,32 @@ const fileUpload = multer({
       cb(null, uuid() + '.' + ext);
     },
   }),
+  
   fileFilter: (req, file, cb) => {
     const isValid = !!MIME_TYPE_MAP[file.mimetype];
     let error = isValid ? null : new Error('Invalid mime type');
     cb(error, isValid);
   },
+  
+});
+const pdfUpload = multer({
+  limit: 500000,
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/pdf');
+    },
+    filename: (req, file, cb) => {
+      const ext = MIME_TYPE_MAP[file.mimetype];
+      cb(null, uuid() + '.' + ext);
+    },
+  }),
+  fileFilter: (req, file, cb) => {
+    const isValid = !!MIME_TYPE_MAP[file.mimetype];
+    let error = isValid ? null : new Error('Invalid mime type');
+    cb(error, isValid);
+  },
+  
 });
 
 module.exports = fileUpload;
+module.exports = pdfUpload;
--- server/routes/Users.js
+++ server/routes/Users.js
@@ -4,6 +4,7 @@ const { Users, Shops, Carts, Contacts } = require('../models');
 const bcrypt = require('bcrypt');
 const { sign } = require('jsonwebtoken');
 const { validateToken } = require('../middlewares/AuthMiddleware');
+const pdfUpload = require('../middlewares/UploadMiddleware');
 
 router.post('/regCustomer', async (req, res) => {
   const { firstName, lastName, mobileNumber, address, email, password } =
@@ -45,47 +46,66 @@ router.post('/regAdmin', async (req, res) => {
   });
 });
 
-router.post('/regShop', async (req, res) => {
-  const {
-    firstName,
-    lastName,
-    mobileNumber,
-    address,
-    email,
-    password,
-    serviceType,
-    shopName,
-  } = req.body;
+router.post('/regShop', pdfUpload.single('file'),
+  async (req, res) => {
 
-  await bcrypt.hash(password, 10).then((hash) => {
-    Users.create({
-      firstName: firstName,
-      lastName: lastName,
-      mobileNumber: mobileNumber,
-      userRole: 'Shop',
-      address: address,
-      email: email,
-      password: hash,
-    }).then((user) => {
-      Shops.create({
-        shopName: shopName,
-        serviceType: serviceType,
-        userId: user.id,
-      });
-      Contacts.create({
-        userId: user.id,
+    const {
+      firstName,
+      lastName,
+      mobileNumber,
+      address,
+      email,
+      password,
+      serviceType,
+      shopName,
+    } = req.body;
+
+    const filePath = req.file.path;
+
+    await bcrypt.hash(password, 10).then((hash) => {
+      Users.create({
+        firstName: firstName,
+        lastName: lastName,
+        mobileNumber: mobileNumber,
+        userRole: 'Shop',
+        address: address,
+        email: email,
+        password: hash,
+      }).then((user) => {
+        Shops.create({
+          shopName: shopName,
+          serviceType: serviceType,
+          userId: user.id,
+          file: filePath,
+          regApproval: false,
+
+        });
+        Contacts.create({
+          userId: user.id,
+        });
       });
     });
+
+    res.json('Success');
   });
 
-  res.json('Success');
-});
+
 
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
   const user = await Users.findOne({ where: { email: email } });
 
   if (!user) res.json({ error: "User doesn't exist" });
+
+  const shop = await Shops.findOne({
+    where: {
+      userId: user.id
+    }
+  })
+
+  if (shop.regApproval === false) {
+    res.json({ error: "Shop registration not approved" });
+  }
 
   bcrypt.compare(password, user.password).then((match) => {
     if (!match) res.json({ error: 'Wrong Username or Password' });
